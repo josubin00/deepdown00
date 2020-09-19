e870cb44ec2ad5ca635ba27740ed93a99c5b8e36
josubin00 <plutojsb@gmail.com>
2020-09-20 01:42:36 +0900

    1st commit

---
+++ js/plugin/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+    $(".scrollBox").niceScroll({
+        cursorcolor:"#777",
+        cursorwidth:5,
+        scrollspeed:40,
+        cursorborderradius:'0',
+        mousescrollstep:40,
+        cursoropacitymin:0,
+        cursoropacitymax:1,
+        background:"#c1c1c1",
+        cursorborder:"none",
+        autohidemode:false,
+        boxzoom:false,
+        smoothscroll:true,
+        zindex:99999
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
